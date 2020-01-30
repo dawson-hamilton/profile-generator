e97672d85ac5184d74df4b6c256a2ce16792c885
PRISON.MIKE <dawsondhamilton@gmail.com>
2020-01-30 10:53:04 -0700

    adding the init function that will be called when the program starts

--- index.js
+++ index.js
@@ -3,7 +3,43 @@ const htmlGen = require("./generateHTML");
 const fs = require("fs");
 const axios = require("axios");
 const util = require("util");
-console.log(htmlGen.colors);
+const pdf = require('html-pdf')
+const writeToFileAsync = util.promisify(fs.writeFile);
+
+async function init() {
+    try {
+        let name = await fullName();
+        let { username } = await getName();
+        let color = await getColor();
+        let githubReturn = await gitHubCall(username);
+        let userStars = await getGithubStarred(username);
+        let userProfile = githubReturn.data
+        let bioBio = await userProfile.bio.split(".");
+        let profileBio = bioBio[0];
+        let profile = {
+            picture: userProfile.avatar_url,
+            blog: userProfile.blog,
+            location: userProfile.location,
+            github: userProfile.html_url,
+            bio: profileBio,
+            followers: userProfile.followers,
+            following: userProfile.following,
+            repos: userProfile.public_repos
+        }
+        let starred = userStars.data.length;
+        let newHtml = htmlGen(color, profile, starred, name)
+        let options = { format: 'A4' };
+        await writeToFileAsync(`${username}.html`, newHtml)
+        var html = await fs.readFileSync(`./${username}.html`, 'utf8')
+        pdf.create(html, options).toFile(`./${username}.pdf`, function (err, res) {
+            if (err) return console.log(err);
+            console.log(res);
+        });
+        console.log("Success!")
+    } catch (err) {
+        console.log(err)
+    }
+}
 
 function promptUser() {
     function getName() {
